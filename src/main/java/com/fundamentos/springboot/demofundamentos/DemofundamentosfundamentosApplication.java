5f54c7d281ef3e14fc1de85bcffed805a3efc187
JosheR <josher.f@gmail.com>
2021-09-20 12:30:48 -0300

    primer commit

---
+++ src/main/java/com/fundamentos/springboot/demofundamentos/DemofundamentosfundamentosApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentos.springboot.demofundamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemofundamentosfundamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemofundamentosfundamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentos/springboot/demofundamentos/DemofundamentosfundamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentos.springboot.demofundamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemofundamentosfundamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
